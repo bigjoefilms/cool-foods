3f932d6d5656023c60fe347f32ad927bd9004dcd
MAC <mac@Nobis-mac.local>
2022-08-31 07:22:31 +0100

    created project folder

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Coolfood } from './components/Coolfood';
+
+const App = () => {
+
+  return (
+    <div className="App">
+    
+      <Coolfood />
+    </div>
+  );
+}
+
+export default App;
